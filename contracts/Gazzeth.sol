4f680f95cc69f5675f01208c29904df590e602b7
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-03-18 00:37:54 -0300

    Gazzeth ERC20 style details

--- contracts/Gazzeth.sol
+++ contracts/Gazzeth.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Gazzeth is ERC20 {
 
-    mapping(address => bool) hasAlreadyClaimed;
-    uint256 public claimAmount = 10;
+    uint8 constant CLAIM_AMOUNT = 10;
 
-    constructor() public ERC20("Gazzeth", "GZT") {
-        _setupDecimals(2);
-        _mint(msg.sender, 0);
+    mapping (address => bool) hasAlreadyClaimed;
+
+    constructor() ERC20("Gazzeth", "GZT") {
+        _setupDecimals(18);
     }
 
    /**
@@ -20,6 +20,6 @@ contract Gazzeth is ERC20 {
     function claim() external {
         require(!hasAlreadyClaimed[msg.sender], "You can only claim for Gazzeths once and you have already done it.");
         hasAlreadyClaimed[msg.sender] = true;
-        _mint(msg.sender, claimAmount);
+        _mint(msg.sender, CLAIM_AMOUNT);
     }
 }
