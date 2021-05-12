72b8bce2542318950b3472e4ab206b5dbeb5c6c1
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-12 01:14:16 -0300

    Some more test added

--- test/Gazzeth.js
+++ test/Gazzeth.js
@@ -2,32 +2,65 @@ const { expect } = require("chai");
 
 describe("Gazzeth token contract", function() {
 
-    let Gazzeth;
-    let gazzeth;
-    let owner;
-    let protocolContract;
-    let nonOwner;
+    describe("Gazzeth from clean deploy", async function() {
 
-    beforeEach(async function() {
-        [owner, nonOwner, protocolContract, ...addresses] = await ethers.getSigners();
-        Gazzeth = await ethers.getContractFactory("Gazzeth");
-        gazzeth = await Gazzeth.deploy();
-    });
+        let Gazzeth;
+        let gazzeth;
+        let owner;
+        let protocolContract;
+        let nonOwner;
 
-    it("Deployment total supply must be zero", async function() {
-        expect(await gazzeth.totalSupply()).to.equal(0);
-    });
+        beforeEach(async function() {
+            [owner, nonOwner, protocolContract, ...addresses] = await ethers.getSigners();
+            Gazzeth = await ethers.getContractFactory("Gazzeth");
+            gazzeth = await Gazzeth.deploy();
+        });
 
-    it("Non owner account can't set protocol address", async function() {
-        await expect(
-            gazzeth.connect(nonOwner).setProtocolContractAddress(protocolContract.address)
-        ).to.be.revertedWith("Only owner can call this function");
-    });
+        it("Deployment total supply must be zero", async function() {
+            expect(await gazzeth.totalSupply()).to.equal(0);
+        });
 
-    it("Owner can set protocol address but only once", async function() {
-        gazzeth.setProtocolContractAddress(protocolContract.address)
-        await expect(
+        it("Non owner account can't set protocol address", async function() {
+            await expect(
+                gazzeth.connect(nonOwner).setProtocolContractAddress(protocolContract.address)
+            ).to.be.revertedWith("Only owner can call this function");
+        });
+
+        it("Owner can set protocol address but only once", async function() {
             gazzeth.setProtocolContractAddress(protocolContract.address)
-        ).to.be.revertedWith("Protocol contract address already set");
+            await expect(
+                gazzeth.setProtocolContractAddress(protocolContract.address)
+            ).to.be.revertedWith("Protocol contract address already set");
+        });
+    });
+
+    describe("Gazzeth when owner has balance greater than zero", async function() {
+
+        let Gazzeth;
+        let gazzeth;
+        let owner;
+        let protocolContract;
+        let other;
+        const quantityToMint = 10000;
+
+        beforeEach(async function() {
+            [owner, other, protocolContract, ...addresses] = await ethers.getSigners();
+            Gazzeth = await ethers.getContractFactory("Gazzeth");
+            gazzeth = await Gazzeth.deploy();
+            gazzeth.setProtocolContractAddress(protocolContract.address)
+            await gazzeth.connect(protocolContract).mint(owner.address, quantityToMint);
+        });
+
+        it("When protocol mints tokens then minted must match supply and balance", async function() {
+            expect(await gazzeth.totalSupply()).to.equal(quantityToMint);
+            expect(await gazzeth.balanceOf(owner.address)).to.equal(quantityToMint);
+        });
+
+        it("When protocol burn tokens then minted must match supply and balance", async function() {
+            const quantityToBurn = 6900;
+            expect(quantityToBurn < quantityToMint);
+            await gazzeth.connect(protocolContract).burn(owner.address, quantityToBurn);
+            expect(await gazzeth.balanceOf(owner.address)).to.equal(quantityToMint - quantityToBurn);
+        });
     });
 });
