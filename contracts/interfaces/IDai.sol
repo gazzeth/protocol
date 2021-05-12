60c0ff0c7ad622b90cbc37ec81402e7c110ef972
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-12 01:22:42 -0300

    Dai interface parameter naming convention

--- contracts/interfaces/IDai.sol
+++ contracts/interfaces/IDai.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IDai is IERC20 {
     
     function permit(
-        address holder, address spender, uint256 nonce, uint256 expiry, bool allowed, uint8 v, bytes32 r, bytes32 s
+        address _holder, address _spender, uint256 _nonce, uint256 _expiry, bool _allowed, uint8 _v, bytes32 _r, bytes32 _s
     ) external;
 }
