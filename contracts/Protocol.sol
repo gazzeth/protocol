4366ae5b9a66a1ff72b6b96737cd10bbd44d7050
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-11 22:47:50 -0300

    Rebirth: DAI as collateral

--- contracts/Protocol.sol
+++ contracts/Protocol.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import { Gazzeth } from "./Gazzeth.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./interfaces/IDai.sol";
+import "./interfaces/IProofOfHumanity.sol";
+import "./Gazzeth.sol";
 
 contract Protocol {
+
+    using SafeMath for uint256;
 
     modifier onlyExistentPublications(uint256 publicationId) {
         require(publicationId < nextPublicationId, "Publication does not exist");
@@ -23,7 +28,6 @@ contract Protocol {
     }
     
     struct Vote {
-        bytes32 commitment;
         VoteValue value;
         string justification;
     }
@@ -34,83 +38,69 @@ contract Protocol {
     }
 
     struct Publication {
-        uint256 id;
         string hash;
         address author;
-        string topic;
-        uint publishDate;
+        string topicId;
+        uint256 publishDate;
         Votation votation;
     }
 
-    uint256 minimumTopicJurorsQuantity;
+    struct Topic {
+        string name;
+        string description;
+        bool active;
+        uint256 publishPrice;
+        uint256 jurorPrice;
+        uint256 voteCommitmentDeadline;
+        uint256 voteRevealDeadline;
+        mapping (address => bool) jurors;
+        uint256 jurorQuantity;
+    }
+
+    Gazzeth gazzeth;
+    IDai dai;
+    IProofOfHumanity proofOfHumanity;
+    uint256 minTopicJurorsQuantity;
     uint256 votingJurorsQuantity;
     uint256 nextPublicationId;
-    mapping (string => address[]) topicJurors;
-    mapping (address => string[]) jurorTopics;
     mapping (uint256 => Publication) publications;
-    Gazzeth gazzeth;
+    mapping (uint256 => Topic) topics;
 
-    constructor(Gazzeth _gazzeth, uint256 _minimumTopicJurorsQuantity, uint256 _votingJurorsQuantity) {
+    constructor(
+        Gazzeth _gazzeth,
+        IDai _dai,
+        IProofOfHumanity _proofOfHumanity,
+        uint256 _minTopicJurorsQuantity,
+        uint256 _votingJurorsQuantity
+    ) {
         gazzeth = _gazzeth;
-        minimumTopicJurorsQuantity = _minimumTopicJurorsQuantity;
+        dai = _dai;
+        proofOfHumanity = _proofOfHumanity;
+        minTopicJurorsQuantity = _minTopicJurorsQuantity;
         votingJurorsQuantity = _votingJurorsQuantity;
     }
 
-    function publish(string calldata _hash, string calldata _topic) external returns (uint256) {
-        // require(check for msg.sender sufficient GZT); 
-        require(topicJurors[_topic].length >= minimumTopicJurorsQuantity, "Insuficient jurors subscribed to the topic");
+    function publish(
+        string calldata _publicationHash,
+        uint256 _topicId,
+        uint256 _nonce,
+        uint256 _expiry,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external returns (uint256) {
+        require(topics[_topicId].active, "Inactive topic");
+        require(topics[_topicId].jurorQuantity >= minTopicJurorsQuantity, "Insuficient jurors subscribed to the topic");
+        require(dai.balanceOf(msg.sender) >= topics[_topicId].publishPrice, "Insuficient DAI to publish");
         Publication storage publication = publications[nextPublicationId];
         publication.id = nextPublicationId;
-        publication.hash = _hash;
+        publication.hash = _publicationHash;
         publication.author = msg.sender;
         publication.publishDate = block.timestamp;
-        publication.topic = _topic;
-        publication.votation.jurors = chooseJurors(_topic);
-        // lock GZT
+        publication.topicId = _topicId;
+        // publication.votation.jurors = chooseJurors(_topic);
+        dai.permit(msg.sender, address(this), _nonce, _expiry, true, _v, _r, _s);
+        dai.transferFrom(msg.sender, address(this), topics[_topicId].publishPrice);
         return nextPublicationId++;
-    }
-
-    function commitVote(
-        uint256 _publicationId, bytes32 _commitment
-    ) external onlyExistentPublications(_publicationId) onlyPublicationJurors(_publicationId) {
-        // require(time for commitment phase has not finished);
-        publications[_publicationId].votation.votes[msg.sender].commitment = _commitment;
-    }
-
-    function revealVote(
-        uint256 _publicationId, VoteValue _voteValue, bytes32 _secret, string calldata _justification
-    ) external onlyExistentPublications(_publicationId) onlyPublicationJurors(_publicationId) {
-        // require(time for reveal phase has not finished);
-        bytes32 rebuiltCommitment = buildCommitment(_voteValue, _secret);
-        require(
-            rebuiltCommitment == publications[_publicationId].votation.votes[msg.sender].commitment,
-            "Reveal didn't match commitment"
-        );
-        publications[_publicationId].votation.votes[msg.sender].justification = _justification;
-    }
-
-    function chooseJurors(string calldata _topic) internal pure returns (address[] memory) {
-        // Chose jurors from topic randomly, mark as "locked" part of their GZT
-    }
-
-    function addressIsJurorInPublication(address _addressToValidate, uint256 _publicationId) public view returns (bool) {
-        for (uint8 i = 0; i < publications[_publicationId].votation.jurors.length; i++) {
-            if (publications[_publicationId].votation.jurors[i] == _addressToValidate) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function buildCommitment(VoteValue _voteValue, bytes32 _secret) internal view returns (bytes32) {
-        // return keccak256(concatStrings(voteValueToString(voteValue), string(secret)));
-    }
-
-    function subscribeAsJurorForTopic(string calldata _topic) external {
-        // Subscribe msg.sender to the given topic. Lock his GZT
-    }
-
-    function unsubscribeAsJurorForTopic(string calldata _topic) external {
-        // Unsubscribe msg.sender to the given topic. Unlock his GZT
     }
 }
