90cbe1d30edd9a9ce7d0c72217a79fbb8ba36ada
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-11 22:53:58 -0300

    Dai interface fix: semicolon added

--- contracts/interfaces/IDai.sol
+++ contracts/interfaces/IDai.sol
@@ -7,5 +7,5 @@ interface IDai is IERC20 {
     
     function permit(
         address holder, address spender, uint256 nonce, uint256 expiry, bool allowed, uint8 v, bytes32 r, bytes32 s
-    ) external
+    ) external;
 }
