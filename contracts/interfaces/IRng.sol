28310db72aa12ee1544d8c332be2f8f071392d67
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2021-05-29 17:57:32 -0300

    Style improvements

--- contracts/interfaces/IRng.sol
+++ contracts/interfaces/IRng.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.7.0;
 interface IRng {
     
     /**
-     * @param _quantity The quantity of random numbers to output
-     * @return A random number array of the given length
+     * @param _quantity The quantity of random numbers to output.
+     * @return A random number array of the given length.
      */
     function getRandomNumbers(uint256 _quantity) external returns (uint256[] memory);
 }
